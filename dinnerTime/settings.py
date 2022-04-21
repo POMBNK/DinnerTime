7743d12d0a6a6d3746e7c2c390ad3a584a1da068
POMBNK <shardex11@gmail.com>
2022-04-21 20:49:05 +0300

    add .gitignore

--- dinnerTime/settings.py
+++ dinnerTime/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b)2$fs-4p*mskjgxlilqse-(^_@*5h_l%+6*m*shux$7!7$6w^'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -132,3 +132,4 @@ COMPRESS_PRECOMPILERS = (
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+from .env import *
